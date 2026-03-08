1e96ebb02648337ef7cb77621d65128ef61fa9a1
Daniel Winterstein <daniel.winterstein@n-able.com>
2026-03-08 22:48:16 +0000

    wip

--- _data/events.js
+++ _data/events.js
@@ -1,26 +1,26 @@
 export default [
   {
     id: 1,
-    title: 'Election Hustings on Digital Tech / AI (Society & Media theme)',
+    title: 'Election Hustings on Digital Tech and AI (Society and Media theme)',
     date: '2026-03-08',
     time: '18:30',
     location: 'Glasgow, UK',
     description:
-      `How is artificial intelligence reshaping society and the media—and what should we do about it? This Scottish Parliament election hustings is a chance to put those questions directly to the people seeking your vote.
+      `How is tech annd AI reshaping society and the media—and what should we do about it? This Scottish Parliament election hustings is a chance to put those questions directly to the people seeking your vote.
 
-The event will bring together politicians and experts to discuss disinformation, digital rights, and the rules that should shape our technological future. RSVP via the link above to secure your place.`,
+The event will bring together politicians and experts to discuss digital media, digital rights, and the rules that should shape our technological future. RSVP via the link above to secure your place.`,
     rsvpUrl: 'https://luma.com/px4pzlsh',
   },
   {
     id: 2,
-    title: 'Election Hustings on Digital Tech / AI (Economy theme)',
+    title: 'Election Hustings on Digital Tech and AI (Economy theme)',
     date: '2026-04-23',
     time: '18:30',
     location: 'Edinburgh: Bayes Centre, University of Edinburgh, 47 Potterrow, Edinburgh, EH8 9BT',
     description:
-      `How do we want digital technologies and AI to serve people, communities, and democracy? ​This Scottish Parliament election hustings is a chance to put those questions directly to the people seeking your vote.
+      `How do we want the digital economy and AI to serve people, communities, and democracy? ​This Scottish Parliament election hustings is a chance to put those questions directly to the people seeking your vote.
 
-​The event will bring together politicians and experts to discuss Scotland's digital economy, digital rights, and the rules that should shape our technological future.`,
+​The event will bring together politicians and experts to discuss Scotland's digital economy, and the rules that should shape our technological future.`,
     rsvpUrl: 'https://luma.com/434lpdao',
   },
   // {
